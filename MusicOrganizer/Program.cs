32433e2eb56aac243a1850d7941ccf0fa71db373
Daniel Brunner <0xFEEDC0DE64@gmail.com>
2017-07-09 12:22:34 +0200

    First tests

---
+++ MusicOrganizer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace MusicOrganizer
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var paths = Directory.GetFiles(@"C:\Users\Daniel\Music\Bollwerk", "*.mp3", SearchOption.AllDirectories);
+            foreach (var path in paths)
+            {
+                var file = TagLib.File.Create(path);
+                Console.WriteLine("{0}BPM: {1}", file.Tag.BeatsPerMinute, Path.GetFileName(path));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
